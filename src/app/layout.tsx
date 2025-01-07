5d532504d850db12f5aa305b1bccc6f606d40091
Oriola121 <muhibudeen.oriola@gmail.com>
2025-01-07 11:06:56 +0100

    fixed captcha issue, should work now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Script
           src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
         />
       </head>
       <body
