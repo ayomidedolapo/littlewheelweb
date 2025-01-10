05ec8b0053ffaf0d4baf6a1033592461cd9a0a23
Oriola121 <muhibudeen.oriola@gmail.com>
2025-01-10 09:33:31 +0100

    changed font to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import NextTopLoader from "nextjs-toploader";
 import Script from "next/script";
 import { Inter } from "next/font/google";
 
-const inter = Inter();
+const inter = Inter({preload: false,});
 
 export const metadata: Metadata = {
   title: "littlewheel-landing",
